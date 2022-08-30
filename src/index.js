1a0143bf2a5b276522f414ed842d4b431268101f
Derek Syme <dereksyme@gmail.com>
2022-08-29 21:23:26 -0400

    made layout page, styling, sidebar links

--- src/index.js
+++ src/index.js
@@ -1,24 +1,97 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React, { useEffect, useState } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+import App from './App'
+import './index.css'
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+const PageLinks = ({text}) => {
+
+  const [ hover, setHover] = useState(false)
+
+  return (
+    <div
+      onClick={() => {
+        
+      }}
+      onMouseEnter={() => {
+        setHover(true)
+      }}
+      onMouseOut={() => {
+        setHover(false)
+      }}
+      style={{ 
+        height: 20,
+        color: 'white',
+        paddingLeft: 20,
+        cursor: 'pointer',
+        fontFamily: 'Fira Sans,Roboto,Arial,Helvetica,sans-serif',
+        paddingTop: 8,
+        color: hover ? 'grey' : 'white'
+      }}
+    >
+      {text}
+    </div>
+  )
+}
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <div style={{ 
+        position: 'absolute',
+        height:'100%', 
+        width:'100%', 
+        backgroundColor: 'grey'
+      }}>
+        {/* Header */}
+        <div style={{
+          position: 'absolute',
+          width: '100%',
+          height: 60,
+          left: 0,
+          top: 0,
+          background: 'white'
+        }}>
+          {/* User */}
+          <div 
+            style={{
+              position: 'absolute',
+              width: 30,
+              height: 30,
+              //left: 0,
+              //top: 0,
+              background: 'grey',
+              borderRadius: 25,
+              marginLeft: '95%',
+              marginTop: 17
+            }}
+          >
+          </div>
+        </div>
+        {/* Sidebar */}
+        <div style={{ 
+          position: 'absolute',
+          width: 95,
+          height: '100%',
+          left: 0,
+          top: 60,
+          paddingTop: 8,
+          background: '#282828'
+        }}>
+          {/* Page Links */}
+          <PageLinks text='Funds'/>
+          <PageLinks text='Charts'/>
+          <PageLinks text='Payday'/>
+        </div>
+      </div>
     </Provider>
   </React.StrictMode>
-);
+)
 
-//test only
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+//take logic out of mobile app, thats the one I want!
