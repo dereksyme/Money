05db3c4063205a4b4c3d0645ce9e4ead4c75c244
Derek Syme <dereksyme@gmail.com>
2022-09-11 10:57:05 -0400

    finished frequency unit tests

--- src/test/frequency.test.js
+++ src/test/frequency.test.js
@@ -1,15 +1,17 @@
 import getAllFrequenciesByFrequencyType from '../App/lib/frequency.js'
-
+let l = console.log
 
 describe('Frequency', () => {
-    
-    const expectedFrequencies = {
-        "weeklyAmount": 100,
-        "biWeeklyAmount": 200,
-        "monthlyAmount": 433.33,
-        "quarterlyAmount": 1300,
-        "annualAmount": 5200
-    }
+    let expectedFrequencies
+    beforeEach(() => {
+        expectedFrequencies = {
+            "weeklyAmount": 100,
+            "biWeeklyAmount": 200,
+            "monthlyAmount": 433.33,
+            "quarterlyAmount": 1300,
+            "annualAmount": 5200
+        }
+    })
 
     it('should return frequencies based on $100 weekly', () => {
         const weeklyFrequency = getAllFrequenciesByFrequencyType('weekly', 100)
@@ -22,7 +24,21 @@ describe('Frequency', () => {
     })
 
     it('should return frequencies based on $433.33 monthly', () => {
-        const weeklyFrequency = getAllFrequenciesByFrequencyType('monthly', 433.333333)
+        const frequencies = getAllFrequenciesByFrequencyType('monthly', 433.33)
+        //rounding required to deal with rounding in constants in calendar conversions file
+        frequencies.annualAmount = Math.ceil(frequencies.annualAmount)
+        frequencies.quarterlyAmount = Math.ceil(frequencies.quarterlyAmount)
+        expect(frequencies).toEqual(expectedFrequencies)
+    })
+
+    it('should return frequencies based on $1300 quarterly', () => {
+        const weeklyFrequency = getAllFrequenciesByFrequencyType('quarterly', 1300)
+        expect(weeklyFrequency).toEqual(expectedFrequencies)
+    })
+
+    it('should return frequencies based on $5200 annually', () => {
+        const weeklyFrequency = getAllFrequenciesByFrequencyType('annually', 5200)
+        l(weeklyFrequency)
         expect(weeklyFrequency).toEqual(expectedFrequencies)
     })
 })
